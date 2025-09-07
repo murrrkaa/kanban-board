200c6ce66c8d71caf66c147dfb7c1b9564c8dd80
Maria Titova <mashababina857@gmail.com>
2025-09-07 16:30:38 +0300

    added base palette

---
+++ frontend/kanban-board/src/shared/ui/colors.ts
@@ -0,0 +1,28 @@
+export const tailwindColors = {
+  blue: {
+    50: "#6AD2FF",
+    100: "#4318FF",
+    200: "#2B3674",
+    300: "#1B2559",
+  },
+  white: {
+    25: "#FFFFFF",
+    50: "#FAFCFE",
+    100: "#F4F7FE",
+    200: "#E9EDF7",
+    300: "#E0E5F2",
+  },
+  gray: {
+    50: "#A3AED0",
+    100: "#B0BBD5",
+    200: "#",
+    300: "#",
+    400: "#",
+    500: "#",
+  },
+  orange: "#EE5D50",
+  lightOrange: "#FEEFEE",
+  yellow: "#FFCE20",
+  green: "#01B574",
+  lightGreen: "#05CD99",
+};
