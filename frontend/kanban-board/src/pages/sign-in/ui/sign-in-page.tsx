68477923c25d2c53f907b3d645fcb83c12397241
Maria Titova <mashababina857@gmail.com>
2025-10-25 16:25:22 +0300

    updated layout

--- frontend/kanban-board/src/pages/sign-in/ui/sign-in-page.tsx
+++ frontend/kanban-board/src/pages/sign-in/ui/sign-in-page.tsx
@@ -9,7 +9,7 @@ export const SignInPage = () => {
         className="flex-1 h-full flex items-center justify-center
 "
       >
-        <div className="flex flex-col gap-[20px] w-[390px]">
+        <div className="flex flex-col gap-[20px] max-w-[390px] w-full m-[20px]">
           <SignInTitle />
           <SignInForm />
         </div>
