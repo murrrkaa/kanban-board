840f3373edd72a83edd2c2d275c291fd5dfe7c74
Maria Titova <mashababina857@gmail.com>
2025-10-26 11:32:09 +0300

    updated use login hook

--- frontend/kanban-board/src/features/sign-in/model/use-login.tsx
+++ frontend/kanban-board/src/features/sign-in/model/use-login.tsx
@@ -3,16 +3,20 @@ import { RoutesEnum } from "@shared/routes";
 import { postLogin } from "@features/sign-in/model/post-login.ts";
 import { setAccessToken } from "@shared/lib/auth.ts";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "@entities/auth/model/use-auth.ts";
 
 export const useLogin = () => {
   const navigate = useNavigate();
+  const { checkAuth } = useAuth();
   return useMutation({
     mutationKey: [RoutesEnum.LOGIN],
     mutationFn: postLogin,
     onError: (error) => console.log(error),
-    onSuccess: (data) => {
+    onSuccess: async (data) => {
       const { token } = data;
       setAccessToken(token);
+
+      await checkAuth();
 
       navigate(RoutesEnum.PROFILE);
     },
