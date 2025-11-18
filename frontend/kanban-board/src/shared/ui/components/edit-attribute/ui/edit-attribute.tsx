499abfd0a6a86b412aa3ea9ce0510d3f63c6ea95
Maria Titova <mashababina857@gmail.com>
2025-11-18 14:32:38 +0300

    created shared edit attribute item

---
+++ frontend/kanban-board/src/shared/ui/components/edit-attribute/ui/edit-attribute.tsx
@@ -0,0 +1,41 @@
+import { type ChangeEvent, type FC, useState } from "react";
+import { Input } from "@shared/ui/components/input";
+import { cn } from "@shared/lib/cn.ts";
+
+interface IAttribute {
+  label: string;
+  value: string;
+  onChange: (value: string) => void;
+}
+
+export const EditAttribute: FC<IAttribute> = ({ label, value, onChange }) => {
+  const [val, setVal] = useState<string>(value);
+  const [editable, setEditable] = useState(false);
+
+  const onChangeValue = (event: ChangeEvent<HTMLInputElement>) => {
+    setVal(event.target.value);
+    onChange(event.target.value);
+  };
+
+  return (
+    <div className="w-full flex flex-row justify-start items-center gap-[32px] h-[35px]">
+      <div className="w-[186px] text-gray-500 font-normal text-[12px]/[14px]">
+        {label}
+      </div>
+      <div
+        className={cn("basis-1/2 w-full", editable && "border border-blue-400")}
+        onClick={() => setEditable(!editable)}
+        onBlur={() => setEditable(false)}
+      >
+        <Input
+          inputSize="small"
+          value={val}
+          classNames={{
+            inputClassName: "rounded-none border-none",
+          }}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => onChangeValue(e)}
+        />
+      </div>
+    </div>
+  );
+};
---
+++ frontend/kanban-board/src/shared/ui/components/edit-attribute/ui/index.ts
@@ -0,0 +1,3 @@
+import { EditAttribute } from "@shared/ui/components/edit-attribute/ui/edit-attribute.tsx";
+
+export { EditAttribute };
