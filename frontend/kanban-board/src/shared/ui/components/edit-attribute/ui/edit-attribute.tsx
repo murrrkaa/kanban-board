c28c52f47e796ea7d17a45f6fe73b48f9072f3b7
Maria Titova <mashababina857@gmail.com>
2025-12-02 12:12:40 +0300

    updated edit attribute

--- frontend/kanban-board/src/shared/ui/components/edit-attribute/ui/edit-attribute.tsx
+++ frontend/kanban-board/src/shared/ui/components/edit-attribute/ui/edit-attribute.tsx
@@ -6,41 +6,59 @@ export interface IAttribute {
   label: string;
   value: string;
   onChange: (value: string) => void;
+  onBlur: (value: string) => void;
+  error?: string;
 }
 
-export const EditAttribute: FC<IAttribute> = ({ label, value, onChange }) => {
+export const EditAttribute: FC<IAttribute> = ({
+  label,
+  value,
+  onChange,
+  error,
+  onBlur,
+}) => {
   const [val, setVal] = useState<string>(value);
   const [editable, setEditable] = useState(false);
 
   const onChangeValue = (event: ChangeEvent<HTMLInputElement>) => {
+    onChange(val);
     setVal(event.target.value);
   };
 
   const onBlurHandler = () => {
     onChange(val);
+    onBlur(val);
     setEditable(false);
   };
 
   return (
-    <div className="w-full flex flex-row justify-start items-center gap-[32px] h-[35px]">
-      <div className="w-[186px] text-gray-500 font-normal text-[12px]/[14px]">
-        {label}
+    <>
+      <div className="w-full flex flex-row justify-start items-center gap-[32px] h-[35px]">
+        <div className="w-[186px] text-gray-500 font-normal text-[12px]/[14px]">
+          {label}
+        </div>
+        <div
+          className={cn(
+            "basis-1/2 w-full",
+            editable && "border border-blue-400",
+          )}
+          onClick={() => setEditable(!editable)}
+          onBlur={onBlurHandler}
+        >
+          <Input
+            placeholder=""
+            inputSize="small"
+            value={val}
+            classNames={{
+              inputClassName: "rounded-none border-none",
+            }}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => onChangeValue(e)}
+          />
+        </div>
       </div>
-      <div
-        className={cn("basis-1/2 w-full", editable && "border border-blue-400")}
-        onClick={() => setEditable(!editable)}
-        onBlur={onBlurHandler}
-      >
-        <Input
-          placeholder=""
-          inputSize="small"
-          value={val}
-          classNames={{
-            inputClassName: "rounded-none border-none",
-          }}
-          onChange={(e: ChangeEvent<HTMLInputElement>) => onChangeValue(e)}
-        />
-      </div>
-    </div>
+      {error && (
+        <div className="text-[12px]/[16px] text-red ml-[218px]">{error}</div>
+      )}
+    </>
   );
 };
