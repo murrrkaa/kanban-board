a6eda1d342a9018a12322262e32750ee8fe82bad
Maria Titova <mashababina857@gmail.com>
2025-12-03 20:50:36 +0300

    updated page wrapper

--- frontend/kanban-board/src/shared/ui/components/page-wrapper/page-wrapper.tsx
+++ frontend/kanban-board/src/shared/ui/components/page-wrapper/page-wrapper.tsx
@@ -8,7 +8,7 @@ interface IPageWrapperProps {
 
 export const PageWrapper: FC<IPageWrapperProps> = ({ title, children }) => {
   return (
-    <div className="my-[50px] mx-[20px] h-full w-full">
+    <div className="mt-[30px] mb-[10px] mx-[20px] h-full w-full">
       <div className="text-[14px]/[24px] font-medium text-gray-700">
         Pages / {title}
       </div>
