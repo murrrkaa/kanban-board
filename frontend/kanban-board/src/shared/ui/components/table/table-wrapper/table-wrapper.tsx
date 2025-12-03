7e9dc0b608e2942e22d01a6c4c4332f7b4824ab6
Maria Titova <mashababina857@gmail.com>
2025-12-03 20:54:49 +0300

    created table wrapper

---
+++ frontend/kanban-board/src/shared/ui/components/table/table-wrapper/table-wrapper.tsx
@@ -0,0 +1,20 @@
+import type { FC, ReactNode } from "react";
+import { Title } from "@shared/ui/components/title";
+
+interface ITableWrapperProps {
+  children: ReactNode;
+  title?: string;
+}
+
+export const TableWrapper: FC<ITableWrapperProps> = ({ children, title }) => {
+  return (
+    <div className="w-full h-[calc(100vh-133px)] bg-white rounded-[20px]">
+      {title && (
+        <Title className="pt-5 pl-7" size="md">
+          {title}
+        </Title>
+      )}
+      {children}
+    </div>
+  );
+};
