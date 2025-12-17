a2fb1ff1fccb625d32dbf5ef943a37b9aa3ada71
Maria Titova <mashababina857@gmail.com>
2025-12-17 14:36:34 +0300

    added leftslot wrapper

--- frontend/kanban-board/src/pages/users/ui/users-page.tsx
+++ frontend/kanban-board/src/pages/users/ui/users-page.tsx
@@ -1,11 +1,15 @@
 import { PageWrapper } from "@shared/ui/components/page-wrapper";
 import { UserTable } from "@widgets/users/user-table/ui";
 import { EditUserDialog } from "@features/user/edit-user/ui";
+import { Button } from "@shared/ui/components/button";
 
 export const UsersPage = () => {
   return (
     <>
-      <PageWrapper title="Users">
+      <PageWrapper
+        title="Users"
+        leftSlot={<Button className="h-[40px]">Добавить пользователя</Button>}
+      >
         <UserTable />
       </PageWrapper>
       <EditUserDialog />
--- frontend/kanban-board/src/shared/ui/components/page-wrapper/page-wrapper.tsx
+++ frontend/kanban-board/src/shared/ui/components/page-wrapper/page-wrapper.tsx
@@ -4,17 +4,29 @@ import { Title } from "@shared/ui/components/title";
 interface IPageWrapperProps {
   title: string;
   children: ReactNode;
+  leftSlot?: ReactNode;
 }
 
-export const PageWrapper: FC<IPageWrapperProps> = ({ title, children }) => {
+export const PageWrapper: FC<IPageWrapperProps> = ({
+  title,
+  children,
+  leftSlot,
+}) => {
   return (
     <div className="mt-[30px] mb-[10px] mx-[20px] h-full w-full">
-      <div className="text-[14px]/[24px] font-medium text-gray-700">
-        Pages / {title}
+      <div className="flex flex-row items-center justify-between">
+        <div className="flex flex-col">
+          <div className="text-[14px]/[24px] font-medium text-gray-700">
+            Pages / {title}
+          </div>
+          <Title size="lg" className="mb-[30px]">
+            {title}
+          </Title>
+        </div>
+
+        {leftSlot}
       </div>
-      <Title size="lg" className="mb-[30px]">
-        {title}
-      </Title>
+
       {children}
     </div>
   );
