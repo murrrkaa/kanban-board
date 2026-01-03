480f48867c1ff0a3072fe9c6af4a0968511a3836
Maria Titova <mashababina857@gmail.com>
2026-01-03 15:28:47 +0300

    updated task store

--- frontend/kanban-board/src/entities/task/model/useTaskStore.tsx
+++ frontend/kanban-board/src/entities/task/model/useTaskStore.tsx
@@ -2,15 +2,19 @@ import { create } from "zustand/react";
 import type { ITask } from "@entities/task/model/types.ts";
 
 interface ITaskStore {
-  task: ITask | null;
-  setTask: (task: ITask | null) => void;
+  tasks: ITask[] | null;
+  setTasks: (tasks: ITask[] | null) => void;
+  selectedTask: ITask | null;
+  setSelectedTask: (task: ITask | null) => void;
   editTaskModal: boolean;
   setEditTaskModal: (addTaskModal: boolean) => void;
 }
 
 export const useTaskStore = create<ITaskStore>((set) => ({
-  task: null,
-  setTask: (task: ITask | null) => set({ task }),
+  tasks: null,
+  setTasks: (tasks: ITask[] | null) => set({ tasks }),
+  selectedTask: null,
+  setSelectedTask: (selectedTask: ITask | null) => set({ selectedTask }),
   editTaskModal: false,
   setEditTaskModal: (editTaskModal: boolean) => set({ editTaskModal }),
 }));
