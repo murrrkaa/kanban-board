a26af5c39b3e65d8e61c4e099915edc2f42422f1
Maria Titova <mashababina857@gmail.com>
2026-01-03 16:02:33 +0300

    updated board query key

--- frontend/kanban-board/src/features/board/board-cards/model/use-get-boards.tsx
+++ frontend/kanban-board/src/features/board/board-cards/model/use-get-boards.tsx
@@ -4,7 +4,7 @@ import { getBoards } from "@features/board/board-cards/model/get-boards.ts";
 
 export const useGetBoards = (filters: object) => {
   return useQuery({
-    queryKey: [RoutesEnum.BOARDS],
+    queryKey: [RoutesEnum.BOARDS, filters],
     queryFn: () => getBoards(filters),
   });
 };
